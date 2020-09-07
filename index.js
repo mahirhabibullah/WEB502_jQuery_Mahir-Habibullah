5a1421d293d17cf3ec31e752382a961ba5fed074
mahirhabibullah <68567873+mahirhabibullah@users.noreply.github.com>
2020-09-08 09:23:37 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+  $(document).ready(function(){
+      $("button").click(function(){
+        var boxWidth = $("#boxDiv").width();
+        var boxHeight = $("#boxDiv").height();
+        $("#width").html("Width: " + boxWidth + ", " + "Height: " + boxHeight);
+        boxWidth = $("#boxDiv").innerWidth();
+        boxHeight = $("#boxDiv").innerHeight();
+        $("#innerwidth").html("Inner Width: " + boxWidth + ", " + "Inner Height: " + boxHeight);
+        boxWidth = $("#boxDiv").outerWidth();
+        boxHeight = $("#boxDiv").outerHeight();
+        $("#outerwidth").html("Outer Width: " + boxWidth + ", " + "Outer Height: " + boxHeight);
+        boxWidth = $("#boxDiv").outerWidth(true);
+        boxHeight = $("#boxDiv").outerHeight(true);
+        $("#outerwidthtrue").html("Outer Width(true): " + boxWidth + ", " + "Outer Height(true): " + boxHeight);
+      });
+  });
